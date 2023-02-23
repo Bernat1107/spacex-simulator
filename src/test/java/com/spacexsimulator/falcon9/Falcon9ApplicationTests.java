54b9d96622f69591fd2e199e42aa5a999c9bf2a8
Bernat1107 <79994724+Bernat1107@users.noreply.github.com>
2023-02-23 18:07:18 +0100

    Update Falcon9ApplicationTests.java

--- src/test/java/com/spacexsimulator/falcon9/Falcon9ApplicationTests.java
+++ src/test/java/com/spacexsimulator/falcon9/Falcon9ApplicationTests.java
@@ -2,5 +2,12 @@ package com.spacexsimulator.falcon9;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.ws.rs.POST;
+
 @SpringBootTest
-class Falcon9ApplicationTests {}
+class Falcon9ApplicationTests {
+
+     POST("https//{api-url}/api/landinglegs/verticallanding")
+
+
+}
