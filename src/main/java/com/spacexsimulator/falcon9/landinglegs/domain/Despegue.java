b842f8de1ab9edd96bebb6e7eda26758f64c29e8
Bernat1107 <79994724+Bernat1107@users.noreply.github.com>
2023-02-23 18:41:24 +0100

    Update Despegue.java

--- src/main/java/com/spacexsimulator/falcon9/landinglegs/domain/Despegue.java
+++ src/main/java/com/spacexsimulator/falcon9/landinglegs/domain/Despegue.java
@@ -1,4 +1,9 @@
 package com.spacexsimulator.falcon9.landinglegs.domain;
 
 public class Despegue {
+
+    private Boolean Leg0;
+    private Boolean Leg90;
+    private Boolean Leg180;
+    private Boolean Leg270;
 }
