a91bcd9e18341f786d6703c4dac1fa2fa4d264fb
Bernat1107 <79994724+Bernat1107@users.noreply.github.com>
2023-02-23 18:37:35 +0100

    Update LandingLegs.java

--- src/main/java/com/spacexsimulator/falcon9/landinglegs/domain/LandingLegs.java
+++ src/main/java/com/spacexsimulator/falcon9/landinglegs/domain/LandingLegs.java
@@ -1,8 +1,20 @@
 package com.spacexsimulator.falcon9.landinglegs.domain;
 
+import com.spacexsimulator.falcon9.mission.application.statesservices.falcon9.VerticalLanding;
+
+import javax.ws.rs.HttpMethod;
+import javax.ws.rs.POST;
+
+
+
 public class LandingLegs {
   private Boolean Leg0;
   private Boolean Leg90;
   private Boolean Leg180;
   private Boolean Leg270;
+  private boolean VerticalLanding = true;
+
+
 }
+
+
