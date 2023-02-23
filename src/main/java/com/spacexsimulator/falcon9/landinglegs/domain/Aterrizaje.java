95251801d2074b009545513247d258af0b956d4b
Bernat1107 <79994724+Bernat1107@users.noreply.github.com>
2023-02-23 18:41:20 +0100

    Update Aterrizaje.java

--- src/main/java/com/spacexsimulator/falcon9/landinglegs/domain/Aterrizaje.java
+++ src/main/java/com/spacexsimulator/falcon9/landinglegs/domain/Aterrizaje.java
@@ -3,7 +3,10 @@ package com.spacexsimulator.falcon9.landinglegs.domain;
 public class Aterrizaje {
 
 
-
+    private Boolean Leg0;
+    private Boolean Leg90;
+    private Boolean Leg180;
+    private Boolean Leg270;
 
 
 
